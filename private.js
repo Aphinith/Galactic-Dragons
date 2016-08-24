ce5b29a7ec8c6247e6110582ccd3ccbe56aa4292
Aralya Phinith <aphinith84@gmail.com>
2016-08-24 15:08:24 -0700

    Remove private.js file

--- private.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  sovereignUserId: "sovereignUser",
-  accessKey: "AKIAIOTHZ6DJPDWZJLKQ",
-  bucketName: "galactic.video",
-  secretAccessKey: "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",
-  endpointLocation: 'https://s3-us-west-2.amazonaws.com',
-  grapheneDB_URI: 'http://sovereign:nxVWNSIGAOKCNZiYyfHi@sovereign.sb05.stations.graphenedb.com:24789'
-}
